79e4769208c4afba49d4d2a21f0da40d38754000
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-02-14 00:25:40 +0530

    added credit

--- main.js
+++ main.js
@@ -4,6 +4,6 @@ axios.get("https://extreme-ip-lookup.com/json/").then(res => {
         data = res.data
         document.getElementById("info").innerHTML = `Cases : ${parseInt(data.cases).toLocaleString()}<br>Today's Cases : ${parseInt(data.todayCases).toLocaleString()}<br>Deaths : ${parseInt(data.deaths).toLocaleString()}
         <br>Today's Deaths : ${parseInt(data.todayDeaths).toLocaleString()}<br>Recovered : ${parseInt(data.recovered).toLocaleString()}<br>Today's Recovered : ${parseInt(data.todayRecovered).toLocaleString()}<br>Critical Cases : ${parseInt(data.critical).toLocaleString()}
-        <br>Population : ${parseInt(data.population).toLocaleString()}<br><a href="https://disease.sh/" target="_blank>Thanks to disease.sh</a>`
+        <br>Population : ${parseInt(data.population).toLocaleString()}<br><a href="https://disease.sh/">Thanks to disease.sh</a>`
     })
 })
