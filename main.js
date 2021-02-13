2d3dbb8bd9b830caa370f293d8556594c965deab
DARKDRAGON532 <69623818+DARKDRAGON532@users.noreply.github.com>
2021-02-14 00:17:39 +0530

    fixed bug

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-axios.get("http://ip-api.com/json").then(res => {
+axios.get("https://extreme-ip-lookup.com/json/").then(res => {
     document.getElementById("location").innerHTML = `Your country is <strong id="country">${res.data.country}</strong>`;
     axios.get(`https://disease.sh/v3/covid-19/countries/${res.data.country}`).then(res => {
         data = res.data
-        document.getElementById("info").innerHTML = `Cases : ${data.cases}<br>Today's Cases : ${data.todayCases}<br>Deaths : ${data.deaths}
-        <br>Today's Deaths : ${data.todayDeaths}<br>Recovered : ${data.recovered}<br>Today's Recovered : ${data.todayRecovered}<br>Critical Cases : ${data.critical}
-        <br>Population : ${data.population}`
+        document.getElementById("info").innerHTML = `Cases : ${parseInt(data.cases).toLocaleString()}<br>Today's Cases : ${parseInt(data.todayCases).toLocaleString()}<br>Deaths : ${parseInt(data.deaths).toLocaleString()}
+        <br>Today's Deaths : ${parseInt(data.todayDeaths).toLocaleString()}<br>Recovered : ${parseInt(data.recovered).toLocaleString()}<br>Today's Recovered : ${parseInt(data.todayRecovered).toLocaleString()}<br>Critical Cases : ${parseInt(data.critical).toLocaleString()}
+        <br>Population : ${parseInt(data.population).toLocaleString()}`
     })
 })
